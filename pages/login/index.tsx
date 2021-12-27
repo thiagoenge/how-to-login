9ff8af48d0ec8b39c398eabce0500b4cc11fea1e
Thiago Enge <thiago.enge@ahazou.com>
2021-12-27 02:45:00 -0300

    add login picture

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react'
+import Image from 'next/image'
 import Login from 'src/components/FormLogin'
 import styles from './Login.module.css'
+import loginPic from 'public/images/login-pic.webp'
 
 const LoginPage = ()=>{
   return(
     <div className={styles.loginPage}>
-      <div className={styles.loginPageHero}></div>
+      <div className={styles.loginPageHero}>
+        <Image 
+          alt="Imagem para um login mais bonito" 
+          src={loginPic} 
+          placeholder="blur"
+          objectFit="cover"
+          objectPosition="50%"
+          layout="fill"
+          priority 
+        />
+      </div>
       <div className={styles.loginPageWrapper}>
         <Login />
       </div>
